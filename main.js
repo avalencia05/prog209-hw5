e9078e75ebb5fd7eb261158994184c718f1d605d
avalencia05 <alejandra.valencia05@outlook.com>
2020-01-30 15:41:40 -0800

    v3

--- main.js
+++ main.js
@@ -4,10 +4,28 @@ document.addEventListener("DOMContentLoaded", function(event){
 
     document.getElementById("deal").addEventListener("click",function(){
         deck.Load();
-        random = Math.floor(Math.random()*52)
+        for (let i = 0; i < 26; i++) {
+            random = Math.floor(Math.random()*52)
+            while(deck.cardArray[random].inuse === true){
+            random = Math.floor(Math.random()*52)
+            }
+            playerArray[i] = deck.cardArray[random];
+            deck.cardArray[random].inuse = true;
+
+            random = Math.floor(Math.random()*52)
+            while(deck.cardArray[random].inuse === true){
+            random = Math.floor(Math.random()*52)
+            }
+            computerArray[i] = deck.cardArray[random];
+            deck.cardArray[random].inuse = true;
+            
+        }
+        document.getElementById("deal").style.visibility = "hidden";
+        document.getElementById("next").style.visibility = "hidden";
+        
     })
 
     document.getElementById("next").addEventListener("click",function(){
-        
+        let 
     })
 })
