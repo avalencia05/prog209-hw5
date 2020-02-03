e1d64f51f6d0a2ec519b8163348062c224ce001f
avalencia05 <alejandra.valencia05@outlook.com>
2020-02-03 00:36:44 -0800

    v6

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ document.addEventListener("DOMContentLoaded", function(){
             }
             computerArray[i] = deck.cardArray[random];
             deck.cardArray[random].inuse = true;
-            
         }
         
         document.getElementById("deal").style.visibility = "hidden";
@@ -30,12 +29,13 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("next").addEventListener("click",function(){
         let turnCount = Number(document.getElementById("TurnCount").innerText);
         let playerScore = Number(document.getElementById("PlayerScore").innerText);
-        let comuterScore = Number(document.getElementById("ComputerScore").innerText);
+        let computerScore = Number(document.getElementById("ComputerScore").innerText);
         let playerCard = document.getElementById("PlayerCard");
         let computerCard = document.getElementById("ComputerCard");
 
         playerCard.innerText = rankGen(playerArray[turnCount].rank) +
             " " + suitGen(playerArray[turnCount].suit);
+
         if (playerArray[turnCount].suit === 1 || playerArray[turnCount].suit === 2) {
             playerCard.className = "blackCard";
         }else{
@@ -50,23 +50,41 @@ document.addEventListener("DOMContentLoaded", function(){
         computerCard.innerText = rankGen(computerCard[turnCount].rank) +
             " " + suitGen(computerCard[turnCount].suit);
         
+    //Deciding the round winner
         if (playerCard[turnCount].rank > computerCard[turnCount].rank) {
             playerScore++;
             document.getElementById("message").innerText = "You won this hand!"  
         }
         if (playerCard[turnCount].rank < computerCard[turnCount].rank) {
             computerScore++;
-            document.getElementById("message").innerText = "The computer won this hand!"      
+            document.getElementById("message").innerText = "You lost this hand."      
         }
         if (playerCard[turnCount].rank === computerCard[turnCount].rank) {
             if (playerCard[turnCount].suit < computerCard[turnCount].suit) {
                 playerScore++;
+                document.getElementById("message").innerText = "You won this hand!"
             }else{
                 computerScore++;
+                document.getElementById("message").innerText = "You lost this hand."                                            
             }
         }
 
-        
+        document.getElementById("PlayerScore").innerText = playerScore;
+        document.getElementById("ComputerScore").innerText = computerScore;
+
+        if (turnCount === 26) {
+            if (playerScore > computerScore){
+                document.getElementById("message").innerText = "Game over. YOU WON!"
+            }
+            if (playerScore < computerScore) {
+                document.getElementById("message").innerText = "Game over. YOU LOST!"
+            }
+            if (playerScore === computerScore){
+                document.getElementById("message").innerText = "Game over. IT'S A TIE!"
+            }
+        }
+        document.getElementById("next").style.visibility = "hidden";
+
     })
 })
     
